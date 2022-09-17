0860f88a6db9467cc73f8ca92e10d22b5201352e
Tolik <ProtorskyAV@yandex.ru>
2022-09-17 19:51:28 +0300

    Add program

---
+++ Work1/Task01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Написать программу, которая из иеющегося массива строк формирует массив из строк,
+//длина которых меньше или равна трем симвалам. Первоначальный массив вводится с клавиатуры.
+//["hello", "2", "world", ":-"] -> ["2", ":-"]
+namespace Task01
+{
+    class Program
+    {
+        static string[] Read(string Caption)
+        {
+            Console.Write($"{Caption}:");
+
+            return Console.ReadLine().Split(',');
+        }
+        static string[] Convert(string[] Array)
+        {
+            return Array.Where(Item=>Item.Length < 4).ToArray();
+        }
+        static void Print(string[] Array)
+        {
+            Console.Write("[{0}]->[{1}]", string.Join(",", Array), string.Join(",", Convert(Array)));
+        }
+        static void Main(string[] args)
+        {
+            Print(Read("Input array"));
+
+            Console.ReadKey();
+        }
+    }
+}
